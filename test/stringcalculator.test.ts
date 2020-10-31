5961542814cfd07fc90ea89bb411d96b71cda6cf
Tom <thomas.spencer@codurance.com>
2020-10-31 12:42:20 +0000

    Add test for multiple numbers

--- test/stringcalculator.test.ts
+++ test/stringcalculator.test.ts
@@ -5,7 +5,8 @@ const each = require("jest-each").default;
 describe("String Calculator", () => {
   each([
     ["1", 1],
-    ["1,2", 3]
+    ["1,2", 3],
+    ["1, 2, 3, 4, 5, 6", 21]
   ]).test("returns %s for %s", (input, expected) => {
     expect(stringCalculator(input)).toEqual(expected);
   });
