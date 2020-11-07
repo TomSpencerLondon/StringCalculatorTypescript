5602bed45ba774a245bd9442c54a8fef25102373
Tom <thomas.spencer@codurance.com>
2020-11-07 12:09:38 +0000

    Add refactoring

--- src/string-calculator.ts
+++ src/string-calculator.ts
@@ -4,30 +4,30 @@ export const stringCalculator = (input: string): number => {
 
 const performAdd = (numbers: number[]) => {
   return numbers.reduce((accumulator, number) => accumulator + number, 0);
-}
+};
 
 // Split the specs into array of numbers
 const stringToNumbers = (args) => {
-  let config = getConfig(args);
-  let numbers = config.input.split("\n");
+  const config = getConfig(args);
+  const numbers = config.input.split("\n");
   return splitOnDelimiters(numbers, config.delimiters);
-}
+};
 
 const splitOnDelimiters = (numbers, delimiters) => {
   if (delimiters.length === 0) {
     return numbers.map(function (num) {
-      return (parseInt(num) || 0); // If +num is NaN be lenient and return 0
+      return parseInt(num) || 0; // If +num is NaN be lenient and return 0
     });
   }
-  let delimiter = delimiters.pop();
-  let newNums = numbers.reduce(function (accumulator, number) {
+  const delimiter = delimiters.pop();
+  const newNums = numbers.reduce(function (accumulator, number) {
     return accumulator.concat(number.split(delimiter));
   }, []);
   return splitOnDelimiters(newNums, delimiters);
-}
+};
 
 const checkNegatives = (numbers) => {
-  let negatives = numbers.filter((num) => {
+  const negatives = numbers.filter((num) => {
     return num < 0;
   });
   if (negatives.length > 0) {
@@ -35,7 +35,7 @@ const checkNegatives = (numbers) => {
   } else {
     return numbers;
   }
-}
+};
 
 function fixLargeNumbers(numbers) {
   return numbers.filter(function (num) {
@@ -44,12 +44,15 @@ function fixLargeNumbers(numbers) {
 }
 
 function getConfig(args) {
-  var matcher = /\/\/(.*?)\n/;
-  var result = matcher.exec(args);
+  const matcher = /\/\/(.*?)\n/;
+  const result = matcher.exec(args);
   if (result) {
-    return {delimiters: getDelimiters(result[1]), input: args.slice(matcher.lastIndex)};
+    return {
+      delimiters: getDelimiters(result[1]),
+      input: args.slice(matcher.lastIndex),
+    };
   } else {
-    return {delimiters: [","], input: args};
+    return { delimiters: [","], input: args };
   }
 }
 
